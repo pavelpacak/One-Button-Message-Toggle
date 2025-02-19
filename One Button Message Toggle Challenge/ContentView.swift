c0f89edc3ba4f338c992f6420a92e44a99eee44a
Pavel Pacák <pavel.pacak@gmail.com>
2025-02-19 14:56:11 +0100

    Finished challenge

--- One Button Message Toggle Challenge/ContentView.swift
+++ One Button Message Toggle Challenge/ContentView.swift
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageString = ""
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            Image(systemName: imageString)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+            
+            Spacer()
+            
+            Button("Press Me!") {
+                let message1 = "You Are Awesome!"
+                let message2 = "You Are Great!"
+                
+                let imageString1 = "sun.max.fill"
+                let imageString2 = "hand.thumbsup"
+                
+                
+                if message == message1 {
+                    message = message2
+                    imageString = imageString2
+                } else {
+                    message = message1
+                    imageString = imageString1
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.orange)
+            
         }
         .padding()
     }
