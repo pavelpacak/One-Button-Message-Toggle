4361fa3c2831cf67807e4d9041434ffc4a326f7d
Pavel Pacák <pavel.pacak@gmail.com>
2025-02-19 14:06:43 +0100

    Initial Commit

---
+++ One Button Message Toggle Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  One Button Message Toggle Challenge
+//
+//  Created by Pavel Pacák on 19.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ One Button Message Toggle Challenge/One_Button_Message_Toggle_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  One_Button_Message_Toggle_ChallengeApp.swift
+//  One Button Message Toggle Challenge
+//
+//  Created by Pavel Pacák on 19.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct One_Button_Message_Toggle_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
